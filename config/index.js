f65e3d5e11dbbcd09182a1e6433e3ea7c7b48035
guowentao <gwem.win@gmail.com>
2018-11-13 10:13:13 +0800

    '静态资源路径错误'

--- config/index.js
+++ config/index.js
@@ -58,7 +58,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../docs'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/douban-vue/',
+    assetsPublicPath: '/',
 
     /**
      * Source Maps
